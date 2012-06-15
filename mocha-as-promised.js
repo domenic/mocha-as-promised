76cef8d5ffd1378113f0695f7b74b307bcc8aee0
Domenic Denicola <domenic@domenicdenicola.com>
2012-06-15 05:32:23 -0400

    First mostly-working draft.

--- mocha-as-promised.js
+++ mocha-as-promised.js
@@ -1,5 +1,33 @@
 "use strict";
 
-module.exports = function () {
+function isPromise(x) {
+    return typeof x.then === "function";
+}
 
+var duckPunchedAlready = false;
+
+module.exports = function (mocha) {
+    if (duckPunchedAlready) {
+        return;
+    }
+    duckPunchedAlready = true;
+
+    var OriginalRunnable = mocha.Runnable;
+    mocha.Runnable = function (title, fn) {
+        function newFn(done) {
+            var retVal = fn(done);
+            if (isPromise(retVal)) {
+                retVal.then(
+                    function () { done(); },
+                    function (reason) { done(reason); }
+                );
+            } else if (originalThisFn.length === 0) {
+                done();
+            }
+        }
+
+        OriginalRunnable.call(this, title, newFn);
+    };
+    mocha.Runnable.prototype = OriginalRunnable.prototype;
+    mocha.Runnable.prototype.constructor = mocha.Runnable;
 };
